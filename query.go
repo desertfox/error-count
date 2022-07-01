4e4536b0c49d0a1cf1ae3c6dc5ab0259730dedb7
Rummy <cerealbh@gmail.com>
2022-06-30 19:57:15 -0500

    Update query.go

--- query.go
+++ query.go
@@ -11,17 +11,18 @@ import (
 
 func doQuery() string {
 	if len(os.Args) > 1 {
-		data, err = ioutil.ReadFile(os.Args[1])
+		data, err := ioutil.ReadFile(os.Args[1])
 		if err != nil {
 			panic(err)
 		}
+		return string(data)
 	}
 
 	c := gograylog.New(os.Getenv("EC_HOST"), os.Getenv("EC_USER"), os.Getenv("EC_PASS"))
 
 	f, _ := strconv.Atoi(freq)
 
-	data, err = c.Execute(os.Getenv("EC_QUERY"), os.Getenv("EC_STREAMID"), []string{"message"}, 10000, f)
+	data, err := c.Execute(os.Getenv("EC_QUERY"), os.Getenv("EC_STREAMID"), []string{"message"}, 10000, f)
 	if err != nil {
 		fmt.Println("Unable to make graylog request", err)
 		return ""
